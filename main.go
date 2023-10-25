f8746e71ef8bed13a90f768db15efe1546e9003a
Kelly Wu <kelly.wu@shoplineapp.com>
2023-10-25 22:08:15 +0800

    refactor: group by routerGroup

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 )
 
 func main() {
-	router := gin.Default()                       // returns a pointer to an Engine instance with embeded RouterGroup
-	router.GET("/products", products.GetProducts) // associate the GET HTTP method and /products path with a handler function
-	router.GET("/products/:id", products.GetProduct)
-	router.POST("/products", products.PostProduct)
-	router.DELETE("/products/:id", products.DeleteProduct)
+	router := gin.Default() // returns a pointer to an Engine instance with embeded RouterGroup
+
+	productRoute := router.Group("/products")
+	{
+		productRoute.GET("", products.GetProducts) // associate the GET HTTP method and /products path with a handler function
+		productRoute.GET("/:id", products.GetProduct)
+		productRoute.POST("", products.PostProduct)
+		productRoute.DELETE("/:id", products.DeleteProduct)
+	}
+
 	router.Run("localhost:8080") // attach the router to an http.Server and start the server
 }
